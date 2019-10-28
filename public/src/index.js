602313af52038dcea9a25ad3a0b049e020086579
Max Antinone <mantinone@gmail.com>
2019-10-28 16:50:58 -0700

    Basic color change test

--- public/src/index.js
+++ public/src/index.js
@@ -1,3 +1,13 @@
 function testing(){
   console.log("Hello");
 }
+
+function changePanelColor(colors = testColors){
+  let panes = document.getElementsByClassName('colorPane')
+  for (var i in panes){
+    panes[i].style.backgroundColor = colors[i]
+  }
+}
+
+
+let testColors = ['#00ffff','#00ff00','#ff00ff','#ff0000','#0000ff']
